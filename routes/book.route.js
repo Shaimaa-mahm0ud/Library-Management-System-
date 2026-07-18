71ad4326a1565e645eae131461891ec5988f0246
Shahdelrohmy <shahd.m.elromhy@gmail.com>
2026-07-18 21:35:20 +0300

    Add authentication module and protect book routes

--- routes/book.route.js
+++ routes/book.route.js
@@ -1,13 +1,15 @@
 const express = require("express")
 const router = express.Router()
 const bookcontroller = require("../controllers/book.controller")
+const authMiddleware = require("../middlewares/auth.middleware");
+const adminMiddleware = require("../middlewares/adminMiddleware");
 
-router.post("/create",bookcontroller.createbook)
+router.post("/create",authMiddleware,adminMiddleware,bookcontroller.createbook)
 router.get("/getall",bookcontroller.getallbooks)
 router.get("/getbyid/:id",bookcontroller.getbyid)
 router.get("/search",bookcontroller.searchbook)
-router.put("/update/:id",bookcontroller.updatebook)
-router.delete("/delete/:id",bookcontroller.deletebook)
+router.put("/update/:id",authMiddleware,adminMiddleware,bookcontroller.updatebook)
+router.delete("/delete/:id",authMiddleware,adminMiddleware,bookcontroller.deletebook)
 
 
 module.exports = router
