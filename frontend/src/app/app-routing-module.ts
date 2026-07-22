605f5d778da2d0161fae5cac9476accbcdcf6e02
Reem Ahmed <areem4780@gmail.com>
2026-07-22 18:45:22 +0300

    Add login and register routes to app routing module

--- frontend/src/app/app-routing-module.ts
+++ frontend/src/app/app-routing-module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
 import { Home } from './pages/home/home';
 import { MyBooks } from './pages/my-books/my-books';
 import { ProfileComponent } from './pages/profile/profile';
 import { AdminBooks } from './pages/admin/admin-books/admin-books';
 import { AddBook } from './pages/admin/add-book/add-book';
 import { EditBook } from './pages/admin/edit-book/edit-book';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
-const routes: Routes = [
+export const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
   {
     path: '',
     component: Home
