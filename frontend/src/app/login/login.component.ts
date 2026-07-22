efb7422e1ad2af47f176ed30e861f198af25241d
Reem Ahmed <areem4780@gmail.com>
2026-07-22 18:29:05 +0300

    Fix import path case for AuthService

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { FormGroup, FormControl, Validators, ReactiveFormsModule } from '@angular/forms'
 import { Router, RouterLink } from '@angular/router'
-import { AuthService } from '../services/auth.service'
+import { AuthService } from '../Services/auth.service'
 import { Login } from '../models/login'
 
 @Component({
